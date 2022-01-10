60b36448c557c6ed9a6708c1220c0c4d918697b4
Clément Chevallier <clementinise@gmail.com>
2022-01-10 19:25:35 +0100

    Initial release

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Clementinise'
+description 'KC Checker - Automatically check update of compatible resources'
+version '1.0'
+
+server_scripts {
+    'checker.lua',
+}
+
+kc_checker 'yes'
+kc_name '^4Self Check'
+kc_github 'https://github.com/clementinise/kc-checker'
